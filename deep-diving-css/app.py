84d6037fbf15b81b99440e9e779abaab727c7da4
Ian Fisher <ian@iafisher.com>
2023-03-30 11:57:18 -0400

    [deep-diving-css] Use generic view in Python to serve example pages

--- deep-diving-css/app.py
+++ deep-diving-css/app.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from flask import Flask, render_template
+from flask import Flask, abort, render_template
 from pygments import highlight as pygments_highlight
 from pygments.lexers import CssLexer, HtmlLexer
 from pygments.formatters import HtmlFormatter
@@ -13,16 +13,27 @@ def index_page():
     return render_template("index.html")
 
 
-@app.route("/header-footer")
-def header_footer_page():
-    css_text = Path("static/header_footer.css").read_text()
+@app.route("/examples/<title>")
+def example_page(title):
+    title = _url_pattern_to_filename(title)
+
+    css_path = Path(f"static/{title}.css")
+    template_path = Path(f"templates/examples/{title}.html")
+    if not css_path.exists() or not template_path.exists():
+        abort(404)
+
+    css_text = css_path.read_text()
     highlighted_css = pygments_highlight(css_text, CssLexer(), HtmlFormatter())
 
-    html_text = Path("templates/header_footer.html").read_text()
+    html_text = template_path.read_text()
     highlighted_html = pygments_highlight(html_text, HtmlLexer(), HtmlFormatter())
 
     return render_template(
-        "header_footer.html",
+        f"examples/{title}.html",
         highlighted_css=highlighted_css,
         highlighted_html=highlighted_html,
     )
+
+
+def _url_pattern_to_filename(s):
+    return s.replace("-", "_")
