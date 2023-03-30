2ed99d82b443a559e7d5c226f9d7c9f23d817ef7
Ian Fisher <ian@iafisher.com>
2023-03-30 11:51:41 -0400

    [deep-diving-css] Display CSS snippets with syntax highlighting

--- deep-diving-css/app.py
+++ deep-diving-css/app.py
@@ -1,6 +1,12 @@
+from pathlib import Path
+
 from flask import Flask, render_template
+from pygments import highlight as pygments_highlight
+from pygments.lexers import CssLexer, HtmlLexer
+from pygments.formatters import HtmlFormatter
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index_page():
@@ -9,4 +15,14 @@ def index_page():
 
 @app.route("/header-footer")
 def header_footer_page():
-    return render_template("header_footer.html")
+    css_text = Path("static/header_footer.css").read_text()
+    highlighted_css = pygments_highlight(css_text, CssLexer(), HtmlFormatter())
+
+    html_text = Path("templates/header_footer.html").read_text()
+    highlighted_html = pygments_highlight(html_text, HtmlLexer(), HtmlFormatter())
+
+    return render_template(
+        "header_footer.html",
+        highlighted_css=highlighted_css,
+        highlighted_html=highlighted_html,
+    )
